a6fe075187a13a1c35c4da1cbaace6402a2784e4
LaNice Powell <lanicemp@aol.com>
2019-07-12 21:35:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,25 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(translator)
+ emoji_hash = YAML.load_file(translator)
+  library_hash = {"get_meaning" => {} , "get_emoticon" => {}}
+ emoji_hash.each do |word, emoticon_array|
+   library_hash["get_meaning"][emoticon_array[1]] = word
+   library_hash["get_emoticon"][emoticon_array[0]] = emoji_hash[word][1]
+  end
+  library_hash
+end
+ 
+
+def get_japanese_emoticon(file_path, emoticon)
+  call_library = load_library(file_path)
+  
+  
+ binding.pry 
+  
 end
 
-def get_japanese_emoticon
+#def get_english_meaning
   # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
+#end
