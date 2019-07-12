a4c92b23ebd83dc79001e2457f6cf3e76235154a
LaNice Powell <lanicemp@aol.com>
2019-07-12 22:21:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   call_library = load_library(file_path)
   result = call_library["get_emoticon"][emoticon]
-  
+  #result ? result :
   
   
   
